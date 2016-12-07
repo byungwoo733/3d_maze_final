3587cf72ae4de17805b1116710fbebc77ef767e0
Byungwoo Kim <kimbyungwoo733@hanmail.net>
2016-12-07 14:53:14 +0900

    Init project settings.

---
+++ Assets/SphereMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public float turnSpeed = 540f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //transform.Translate (0f, 0f, h * moveSpeed * Time.deltaTime);
+
+        //Move 
+        transform.Translate(0f, 0f, v * moveSpeed * Time.deltaTime);
+
+        //Turn
+        transform.Rotate(0f, h * turnSpeed * Time.deltaTime, 0f);
+    }
+}
---
+++ Assets/character/cha_knight/models/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+    //public float moveRot = 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+    }
+}
